70a04f1dc9cd748700aa151e281dd693ae2405a1
Karthiga Sree Suresh <karthigasreesuresh@gmail.com>
2026-05-29 15:20:11 +0530

    Connect frontend to Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://planiq-888q.onrender.com/api';
 
 async function apiRequest(endpoint, options = {}) {
   const token = localStorage.getItem('planiq_token');
